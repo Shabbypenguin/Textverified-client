52b5e45bb179bac897611f22037fce760d5538cd
David Rouleau <Shabbypenguin@gmail.com>
2020-01-01 21:46:08 -0500

    Add in json support

---
+++ TextVerified/ApiKey.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TextVerified
+{
+    class ApiKey
+    {
+        public string apikey { get; set; }
+
+        public string apisecret { get; set; }
+
+        public static void WriteJson()
+        {
+            List<ApiKey> _data = new List<ApiKey>();
+            _data.Add(new ApiKey()
+            {
+                apikey = "test",
+                apisecret = "test"
+            });
+
+            using (StreamWriter file = File.CreateText("API.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;
+                serializer.Serialize(file, _data);
+            }
+        }
+
+        public static dynamic ReadJson()
+        {
+            using (StreamReader r = new StreamReader("API.json"))
+            {
+                string json = r.ReadToEnd();
+                List<ApiKey> APIInfo = JsonConvert.DeserializeObject<List<ApiKey>>(json);
+                return APIInfo;
+            }
+            
+        }
+
+    }
+}
--- TextVerified/Form1.cs
+++ TextVerified/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -16,16 +17,36 @@ namespace TextVerified
         public Form1()
         {
             InitializeComponent();
+            populate();
         }
         public static string tokenkey = null;
         public bool smsvalue = true;
+        
 
         private void getBalance_Click(object sender, EventArgs e)
         {
+            populate();
             tokenkey = Program.Base64Encode(apiKey.Text + ":" + apiSecret.Text);
             string token = TextverifiedAPI.DoAuth("https://www.textverified.com/api/Authentication", tokenkey);
             dynamic response = TextverifiedAPI.DoPost("https://www.textverified.com/api/Users", token, "GET");
             outputBox.Text = "Balance is: " + response.credit_balance;
+        }
+
+        private void populate()
+        {
+            if (File.Exists("API.json"))
+            {
+                dynamic apiinfo = ApiKey.ReadJson();
+                apiKey.Text = apiinfo[0].apikey.ToString();
+                apiSecret.Text = apiinfo[0].apisecret.ToString();
+            }
+            else
+            {
+                MessageBox.Show("API.json created, Please edit and rerun the bot.");
+                ApiKey.WriteJson();
+                Environment.Exit(0);
+            }
+
         }
 
         private void smsMode_SelectedIndexChanged(object sender, EventArgs e)
